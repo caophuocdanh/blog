fb5cfb9f5d47597c307638990ea406967c849819
caophuocdanh <haibaraken@gmail.com>
2015-08-23 14:45:00 +0700

    ...

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,229 @@
-@extends('app')
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Trang Chủ</title>
 
-@section('content')
-<div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
+		<!-- Bootstrap CSS -->
+		<link href="/css/bootstrap.min.css" rel="stylesheet">
+		<link rel="stylesheet/less" href="/css/style.less">
 
-				<div class="panel-body">
-					You are logged in!
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<nav class="navbar navbar-default" role="navigation">
+			<div class="container">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="/">Simple Blog</a>
+				</div>
+			
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse navbar-ex1-collapse">
+					<ul class="nav navbar-nav navbar-right">
+						@if (!Session::get('user'))
+							<li class=""><a href="/">Đăng kí</a></li>
+							<li class=""><a href="/">Đăng nhập</a></li>
+						@else
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+								<ul class="dropdown-menu">
+									<li><a href="#">Quản lí tài khoản</a></li>
+									<li><a href="#">Thay đổi mật khẩu</a></li>
+									<li><a href="#">Thoát</a></li>
+								</ul>
+							</li>
+						@endif
+						<li class=""><a href="#">Tuyển dụng</a></li>
+						<li class=""><a href="#">Liên hệ</a></li>
+						<form class="navbar-form navbar-left" role="search">
+							<div class="form-group">
+								<input type="text" class="form-control" placeholder="Search">
+							</div>
+							<button type="submit" class="btn btn-default">Submit</button>
+						</form>
+					</ul>
 				</div>
 			</div>
-		</div>
-	</div>
-</div>
-@endsection
+		</nav>
+		<header id="header">
+			<!-- <div id="carousel-id" class="carousel slide" data-ride="carousel">
+				<ol class="carousel-indicators">
+					<li data-target="#carousel-id" data-slide-to="0" class=""></li>
+					<li data-target="#carousel-id" data-slide-to="1" class=""></li>
+					<li data-target="#carousel-id" data-slide-to="2" class="active"></li>
+				</ol>
+				<div class="carousel-inner">
+					<div class="item">
+						<img data-src="holder.js/1000x400/auto/#777:#7a7a7a/text:First slide" alt="First slide" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI5MDAiIGhlaWdodD0iNTAwIj48cmVjdCB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzc3NyI+PC9yZWN0Pjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjQ1MCIgeT0iMjUwIiBzdHlsZT0iZmlsbDojN2E3YTdhO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjU2cHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+Rmlyc3Qgc2xpZGU8L3RleHQ+PC9zdmc+">
+						<div class="container">
+							<div class="carousel-caption">
+								<h1>Example headline.</h1>
+								<p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
+								<p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<img data-src="holder.js/1000x400/auto/#666:#6a6a6a/text:Second slide" alt="Second slide" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI5MDAiIGhlaWdodD0iNTAwIj48cmVjdCB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzY2NiI+PC9yZWN0Pjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjQ1MCIgeT0iMjUwIiBzdHlsZT0iZmlsbDojNmE2YTZhO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjU2cHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+U2Vjb25kIHNsaWRlPC90ZXh0Pjwvc3ZnPg==">
+						<div class="container">
+							<div class="carousel-caption">
+								<h1>Another example headline.</h1>
+								<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+								<p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+							</div>
+						</div>
+					</div>
+					<div class="item active">
+						<img data-src="holder.js/1000x400/auto/#555:#5a5a5a/text:Third slide" alt="Third slide" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI5MDAiIGhlaWdodD0iNTAwIj48cmVjdCB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzU1NSI+PC9yZWN0Pjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjQ1MCIgeT0iMjUwIiBzdHlsZT0iZmlsbDojNWE1YTVhO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjU2cHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+VGhpcmQgc2xpZGU8L3RleHQ+PC9zdmc+">
+						<div class="container">
+							<div class="carousel-caption">
+								<h1>One more for good measure.</h1>
+								<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+								<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+				<a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+			</div> -->
+		</header>
+		<section id="main-container" class="container">
+			<div class="row">
+				<div class="col-left col-sm-5 col-md-3">
+					<div class="panel panel-primary">
+						<div class="panel-heading">
+							<h3 class="panel-title">Danh Mục</h3>
+						</div>
+						<ul class="list-group">
+								<li class="list-group-item">Cras justo odio <span class="badge">42</span></li>
+								<li class="list-group-item">Dapibus ac facilisis in <span class="badge">42</span></li>
+								<li class="list-group-item">Morbi leo risus <span class="badge">42</span></li>
+								<li class="list-group-item">Porta ac consectetur ac <span class="badge">42</span></li>
+								<li class="list-group-item">Vestibulum at eros <span class="badge">42</span></li>
+							</ul>
+					</div>
+					<div class="panel panel-primary">
+						<div class="panel-heading">
+							<h3 class="panel-title">Note</h3>
+						</div>
+						<div class="panel-body">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis ad provident, ratione quidem quia repellat, iure magnam ipsum nisi voluptates?</p>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo dicta nam quidem unde provident libero fuga natus quos esse, earum neque fugit, rem, nostrum maiores.</p>
+						</div>
+					</div>
+					<div class="panel panel-primary">
+						<div class="panel-heading">
+							<h3 class="panel-title">Quảng Cáo</h3>
+						</div>
+						<div class="thumbnail">
+							<img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI5MDAiIGhlaWdodD0iNTAwIj48cmVjdCB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzY2NiI+PC9yZWN0Pjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjQ1MCIgeT0iMjUwIiBzdHlsZT0iZmlsbDojNmE2YTZhO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjU2cHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+U2Vjb25kIHNsaWRlPC90ZXh0Pjwvc3ZnPg==" alt />
+						</div>
+					</div>
+				</div>
+				<div class="col-right col-sm-7 col-md-9">
+					<!-- div.col-md-6*15>(h3>lorem8)+(p>lorem35)+a[href="#$$"]{readmore} -->
+					<div class="articles">
+						<div class="col-md-6">
+							<h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta culpa impedit consequatur aliquid itaque totam, sapiente iste facilis. Totam expedita perferendis eveniet! Nostrum mollitia quod quas unde alias similique facilis, quia, assumenda expedita deleniti numquam!</p>
+							<a href="#01">readmore</a>
+						</div>
+						<div class="col-md-6">
+							<h3>Temporibus facere eveniet a tenetur, quidem tempora aliquam!</h3>
+							<p>Vero qui facilis eos, provident labore, totam, eius deserunt voluptates iusto, placeat fugiat dolore culpa cum voluptatibus veritatis repellendus ratione vitae assumenda! In molestiae atque commodi ipsam totam libero eos labore modi. Voluptatem, id suscipit.</p>
+							<a href="#02">readmore</a>
+						</div>
+						<div class="col-md-6">
+							<h3>Iste autem eveniet accusamus ea omnis adipisci nulla.</h3>
+							<p>Ad atque est dolores vel dolor tempore. Nam labore voluptatibus repellat impedit, quidem optio ut eius est culpa temporibus repellendus, libero fugiat quos! Natus suscipit tempora unde porro at corporis aliquid voluptatem tenetur dolor, est.</p>
+							<a href="#03">readmore</a>
+						</div>
+						<div class="col-md-6">
+							<h3>Repellat eum laudantium doloremque expedita reprehenderit sapiente, in.</h3>
+							<p>Dolore asperiores ut maiores. Fuga quam libero mollitia quos, enim provident, sed iure nemo incidunt numquam dolore iste est! Odit quam commodi libero dicta, quo veritatis, maiores consectetur est omnis, dolorem animi ipsam eligendi eaque.</p>
+							<a href="#04">readmore</a>
+						</div>
+						<div class="col-md-6">
+							<h3>Nihil voluptate, ducimus repellat quia ipsam inventore eaque?</h3>
+							<p>Aliquid accusamus vitae laborum quidem id deleniti a tenetur minima reprehenderit dolore aut accusantium, velit commodi, nihil dicta dolorum ullam. Culpa dolorum consectetur ducimus, sed praesentium, suscipit in totam repellendus voluptatibus impedit. Id nam, aperiam?</p>
+							<a href="#05">readmore</a>
+						</div>
+						<div class="col-md-6">
+							<h3>Consequatur, quidem fugiat fuga tempora dicta explicabo ratione!</h3>
+							<p>Illum aliquid voluptatum rerum ut nesciunt aut doloremque impedit veritatis obcaecati, aspernatur nulla eveniet ab dolores ad animi ratione autem vel nihil eos accusamus debitis corporis! Omnis ab nemo molestiae unde delectus, similique inventore vero.</p>
+							<a href="#06">readmore</a>
+						</div>
+						<div class="col-md-6">
+							<h3>Dolor vitae, in iure incidunt eveniet ab nam.</h3>
+							<p>Nam aliquam, ipsum rem ratione blanditiis quia, ullam iure accusantium voluptas ipsam sunt. Est eveniet officiis, corporis vel, officia sint veniam, quaerat fuga atque error aliquid. Impedit odit eligendi reprehenderit accusantium possimus quia totam, ut.</p>
+							<a href="#07">readmore</a>
+						</div>
+						<div class="col-md-6">
+							<h3>Accusantium, laboriosam rem harum nulla dolor perspiciatis, eius.</h3>
+							<p>Iusto cum aut vero esse omnis temporibus eveniet culpa ex neque distinctio eos officia ratione delectus est modi quisquam, natus beatae hic inventore reiciendis! Dolore quasi numquam ab error pariatur dolores minus repudiandae! Ullam, fugit.</p>
+							<a href="#08">readmore</a>
+						</div>
+						<div class="col-md-6">
+							<h3>Id aliquam quasi atque, commodi exercitationem minima ullam!</h3>
+							<p>Consectetur provident possimus nostrum illum assumenda molestias, reprehenderit veritatis, ea ad doloribus corrupti vel odio voluptatem nemo natus quos explicabo ipsam illo hic ut magni blanditiis? Adipisci optio deleniti nobis obcaecati explicabo eum ullam! Officia!</p>
+							<a href="#09">readmore</a>
+						</div>
+						<div class="col-md-6">
+							<h3>Voluptates officia aut fugit qui dolores, sit laboriosam.</h3>
+							<p>Magnam laudantium molestiae magni id alias veniam, ea iste iure repellendus non officia, velit sit quo accusamus ratione saepe excepturi perferendis. Minus quae nobis labore error molestias aliquam similique doloremque cum. Aspernatur eum, quae cupiditate.</p>
+							<a href="#10">readmore</a>
+						</div>
+						<div class="col-md-6">
+							<h3>Optio tempora ab rerum, quidem distinctio quod libero.</h3>
+							<p>Nobis nihil dignissimos cupiditate animi suscipit nesciunt autem, vero, fugit nostrum sint quasi ipsa corporis explicabo aliquam totam cumque ab porro vitae, repudiandae aperiam officia excepturi. Nesciunt, est! Delectus quo, eveniet voluptatem doloribus aliquid iusto!</p>
+							<a href="#11">readmore</a>
+						</div>
+						<div class="col-md-6">
+							<h3>Omnis ipsum quia dolore magni rerum commodi neque?</h3>
+							<p>Ullam, molestiae iusto assumenda voluptates doloribus quo et odit, officiis quidem soluta quod voluptate veniam! Earum alias est aliquid eligendi minus recusandae voluptatum nobis, cum saepe aut consectetur, accusamus adipisci accusantium perferendis, expedita deleniti doloremque.</p>
+							<a href="#12">readmore</a>
+						</div>
+					</div>
+					<nav>
+						<ul class="pagination pagination-sm pull-right">
+							<li><a href="#" aria-label="Previous"><span aria-hidden="true">«</span></a></li>
+							<li class="active"><a href="#">1</a></li>
+							<li><a href="#">2</a></li>
+							<li><a href="#">3</a></li>
+							<li><a href="#">4</a></li>
+							<li><a href="#">5</a></li>
+							<li><a href="#" aria-label="Next"><span aria-hidden="true">»</span></a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</section>
+
+		<footer id="footer">
+			<div class="container text-center">
+				<p>Copyright &copy; 2015</p>
+			</div>
+		</footer>
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="/js/bootstrap.min.js"></script>
+		<script src="/js/less.min.js"></script>
+	</body>
+</html>
